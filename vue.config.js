aa32839c4e6d23126984c97d2ed6e4db550881d4
KaixLiu <2250832184@qq.com>
2023-05-09 11:58:04 +0800

    最初

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: process.env.NODE_ENV === 'prduction' ? 'my_web' : './',
-  publicPath: 'my_web',
+  publicPath: './',
   chainWebpack: (config) => {
     // 发布模式
     config.when(process.env.NODE_ENV === 'production', (config) => {
