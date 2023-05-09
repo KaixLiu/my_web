0dbf686376c75450d25a8178e19baa05d0559962
KaixLiu <2250832184@qq.com>
2023-05-09 11:10:02 +0800

    修改路径测试2

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'prduction' ? 'my_web' : './',
-  // publicPath: './',
+  // publicPath: process.env.NODE_ENV === 'prduction' ? 'my_web' : './',
+  publicPath: 'my_web',
   chainWebpack: (config) => {
     // 发布模式
     config.when(process.env.NODE_ENV === 'production', (config) => {
